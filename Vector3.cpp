5a75734d17552e914a47ae8e5e6ade00a56202e8
Juan Mireles <juancnmireles@gmail.com>
2023-02-22 00:00:23 -0500

    Added functions to Vector3 Class

--- Vector3.cpp
+++ Vector3.cpp
@@ -4,21 +4,59 @@
 
 #include "Vector3.h"
 
-Vector3::Vector3() : e{0, 0, 0} {}
-
-Vector3::Vector3(double e0, double e1, double e2) : e{e0, e1, e2} {}
-
-double Vector3::X() const { return e[0]; }
-double Vector3::Y() const { return e[1]; }
-double Vector3::Z() const { return e[2]; }
-
-Vector3 Vector3::operator-() const { return Vector3(-e[0], -e[1], -e[2]); }
-double Vector3::operator[](int i) const { return e[i]; }
-double& Vector3::operator[](int i) { return e[i]; }
 Vector3& Vector3::operator+=(const Vector3& otherVector)
 {
     e[0] += otherVector.e[0];
     e[1] += otherVector.e[1];
     e[2] += otherVector.e[2];
     return *this;
+}
+
+Vector3& Vector3::operator*=(const double t)
+{
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+
+Vector3& Vector3::operator/=(const double t)
+{
+    return *this *= 1/t;
+}
+
+double Vector3::LengthSquared() const
+{
+    return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+}
+
+double Vector3::Length() const
+{
+    return sqrt(LengthSquared());
+}
+
+/* Vector3 Utility Functions */
+inline std::ostream& operator<<(std::ostream& out, const Vector3& otherVector)
+{
+    return out << otherVector.e[0] << ' ' << otherVector.e[1] << ' ' << otherVector.e[2];
+}
+
+inline Vector3 operator+(const Vector3& u, const Vector3& otherVector)
+{
+    return Vector3(u.e[0] + otherVector.e[0], u.e[1] + otherVector.e[1], u.e[2] + otherVector.e[2]);
+}
+
+inline Vector3 operator-(const Vector3& u, const Vector3& otherVector)
+{
+    return Vector3(u.e[0] - otherVector.e[0], u.e[1] - otherVector.e[1], u.e[2] - otherVector.e[2]);
+}
+
+inline Vector3 operator*(const Vector3& u, const Vector3& otherVector)
+{
+    return Vector3(u.e[0] * otherVector.e[0], u.e[1] * otherVector.e[1], u.e[2] * otherVector.e[2]);
+}
+
+inline Vector3 operator*(double t, const Vector3& otherVector)
+{
+    return Vector3(t*otherVector.e[0], t*otherVector.e[1], t*otherVector.e[2]);
 }
--- Vector3.h
+++ Vector3.h
@@ -12,19 +12,35 @@ class Vector3
 {
     public:
         double e[3];
+
     public:
-        Vector3();
-        Vector3(double e0, double e1, double e3);
+        Vector3() : e{0, 0, 0} {}
+        Vector3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        double X() const;
-        double Y() const;
-        double Z() const;
+        double X() const { return e[0]; }
+        double Y() const { return e[1]; }
+        double Z() const { return e[2]; }
 
-        Vector3 operator-() const;
-        double operator[](int i) const;
-        double& operator[](int i);
+        Vector3 operator-() const { return Vector3(-e[0], -e[1], -e[2]); }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
+
         Vector3& operator+=(const Vector3& otherVector);
+        Vector3& operator*=(const double t);
+        Vector3& operator/=(const double t);
+        double LengthSquared() const;
+        double Length() const;
+
+        /* Vector3 Utility Functions */
+        friend inline std::ostream& operator<<(std::ostream &out, const Vector3 &otherVector);
+        friend inline Vector3 operator+(const Vector3& u, const Vector3& otherVector);
+        friend inline Vector3 operator-(const Vector3& u, const Vector3& otherVector);
+        friend inline Vector3 operator*(const Vector3& u, const Vector3& otherVector);
+        friend inline Vector3 operator*(double t, const Vector3& otherVector);
 };
 
+// Type aliases for Vector3
+using point3 = Vector3;    // 3D point
+using color = Vector3;    // RGB color
 
 #endif //RAYTRACING_IN_A_WEEKEND_VECTOR3_H
